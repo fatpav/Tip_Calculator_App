8d5c6a2d16dfa9a14727870a45803ec3aa85ea55
fatpav <paul.donegan@strawberry.co.uk>
2021-09-20 16:50:51 +0100

    more css

---
+++ tipCalc.js
@@ -0,0 +1,8 @@
+function calculateTip(billTotal, percentage) {
+    const result = (billTotal * percentage)
+    console.log(result)
+}
+
+
+// CAalculator has to take BILL TOTAL BEFORE TIP and ADD the given percentage of the initial bill 
+// Must then divide the total bill among the number of people calculate 
